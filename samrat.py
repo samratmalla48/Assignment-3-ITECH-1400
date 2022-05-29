6dfba429df32e242ee437a50f53a4bd134d1a84d
samrat malla <samratmalla48@gmail.com>
2022-05-29 14:07:33 +1000

    Add files via upload

---
+++ samrat.py
@@ -0,0 +1,10 @@
+MAX_WEIGHT_CAP = 10000
+MAX_BASKET_WEIGHT = 25000
+MIN_EATING_WEIGHT = 501
+FISH_TYPES = ['AustralianBass','ShortFinnedEel','EelTailedCatfish','GippslandPerch']
+
+class AustralianBass:
+    Max_weight = 4000
+    Max_Eating_Weight = 2500
+    NAME = 'Australian Bass'
+    LATIN_NAME
